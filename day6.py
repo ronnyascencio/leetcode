3d8b68039cfecf30cb572223ebee3da648fdf519
Ronny Ascencio <ronnycompartist@gmail.com>
2025-04-12 10:04:09 -0400

    rotating a list day 6

---
+++ day6.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)  # how many items ar ein the list
+        k = (
+            k % n
+        )  # we need to neutralize k to tel it that could not be grater than the list  lengh
+        nums[:] = (
+            nums[-k:] + nums[:-k]
+        )  # rotating the list values  taking the last 3 to place in front  taking as a second imput the first elements but not the last3
+        return nums
+
+
+nums = [1, 2, 3, 4, 5, 6, 7]
+k = 3
+solution = Solution()
+print(f"solution: {solution.rotate(nums=nums, k=k)}")
